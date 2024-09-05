38ff4ff2afa57acad031925f2d93f9d995f5d7de
Toadstool2005 <88195149+Toadstool2005@users.noreply.github.com>
2024-09-05 17:04:37 +0200

    Update ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,1 +1,14 @@
+module.exports = async (client, guild) => {
+    client.user.setActivity('Le Royaume Champignon', { type: 'LISTENING' });
+    console.log(`[Discord API] Connecté en tant que ${client.user.username}`.magenta);
 
+    client.guilds.cache.forEach(async (guild) => {
+        try {
+            await guild.members.fetch(); // Mettre en cache tous les membres de la guild
+            console.log(`Tous les membres de ${guild.name} ont été mis en cache.`);
+        } catch (error) {
+            console.error(`Erreur lors de la mise en cache des membres de ${guild.name}:`, error);
+        }
+    });
+
+};
