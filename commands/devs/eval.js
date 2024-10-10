71b453eb65d5ff3a03596eb9b274209495b9cd81
Toadstool2005 <88195149+Toadstool2005@users.noreply.github.com>
2024-10-10 08:29:48 +0200

    Update eval.js

--- commands/devs/eval.js
+++ commands/devs/eval.js
@@ -1,8 +1,10 @@
-const OwnerID = ['777848459855134760'];
+const OwnerID = ['**id de la princesse**'];
+
+// Attention, cette commande est extrêmement dangereuse, songez à vérifier que seulement vous avez accès à cette commande.
 
 module.exports = {
 	name: 'eval',
-	description: 'eval',
+	description: 'Lance un morceux de code',
 	run: async (client, message, args) => {
 		if (!OwnerID.includes(message.author.id)) return;
 		let codein = args.join(' ');
