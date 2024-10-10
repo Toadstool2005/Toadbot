f09d67e37fd1ccd391e54c62d4e35fd57c99bb67
Toadstool2005 <88195149+Toadstool2005@users.noreply.github.com>
2024-10-10 08:11:57 +0200

    Create channelCreate.js

---
+++ events/guild/channelCreate.js
@@ -0,0 +1,39 @@
+const { MessageEmbed } = require('discord.js');
+
+module.exports = async (client, channel) => {
+    try {
+        // Fetch the audit logs to find out who created the channel
+        const auditLogs = await channel.guild.fetchAuditLogs({ type: "CHANNEL_CREATE", limit: 1 });
+        const logEntry = auditLogs.entries.first();
+
+        if (!logEntry) {
+            return;
+        }
+
+        const { executor } = logEntry;
+
+        // Définir où le message embed sera envoyé
+        const Channel = client.channels.cache.get(config.ChannelID);
+
+        // Create the embed message
+        const embed = new MessageEmbed()
+            .setColor('#056dad')
+            .setTitle('Nouveau Salon a été créé')
+            .setAuthor({ name: channel.guild.name, iconURL: channel.guild.iconURL() })
+            .setThumbnail(channel.guild.iconURL({ dynamic: true }))
+            .addFields(
+                { name: '**🍄 Nom du salon :**', value: `<#${channel.id}>` },
+                { name: '**:id: ID du salon :**', value: `${channel.id}` },
+                { name: '**⚙ Type de salon :**', value: `${channel.type}` },
+                { name: '**🚨 Modérateur :**', value: `<@${executor.id}> (\`${executor.id}\`)` },
+            )
+            .setTimestamp()
+            .setFooter({ text: executor.username, iconURL: executor.displayAvatarURL({ dynamic: true }) });
+
+        // Envoie du message dans le salon
+        await Channel.send({ embeds: [embed] });
+      
+    } catch (error) {
+        console.error('Une erreur s’est produite lors de la gestion de l’événement de création de salonS :', error);
+    }
+};
