b994b0286258537d89defe3bb18d2114932cc55b
Toadstool2005 <88195149+Toadstool2005@users.noreply.github.com>
2024-10-10 08:39:51 +0200

    Create emojiCreate.js

---
+++ events/guild/emojiCreate.js
@@ -0,0 +1,27 @@
+const { MessageEmbed } = require('discord.js');
+const config = require("../../config.json");
+
+module.exports = async (client, emoji) => {
+    try {
+        // Définir où le message embed sera envoyé
+        const Channel = client.channels.cache.get(config.ChannelID);
+
+        const fetchEmojiAuthor = await emoji.fetchAuthor();
+        const embed = new MessageEmbed()
+            .setAuthor({ name: emoji.guild.name, iconURL: emoji.guild.iconURL() })
+            .setColor('#056dad')
+            .setTitle('Emoji créé.')
+            .setDescription(`**🍄 ${fetchEmojiAuthor} a créé <:${emoji.name}:${emoji.id}> !**`)
+            .setThumbnail(emoji.url)
+            .setFooter({ text: fetchEmojiAuthor.username, iconURL: fetchEmojiAuthor.displayAvatarURL({ dynamic: true }) })
+            .setTimestamp()
+            .addFields(
+                { name: "🚨 Modérateur :", value: `<@${fetchEmojiAuthor.id}>` }
+            );
+
+            // Envoie du message dans le salon
+        await Channel.send({ embeds: [embed] });
+    } catch (error) {
+        console.error('Une erreur est survenue lors du traitement de la création de l\'emoji :', error);
+    }
+};
